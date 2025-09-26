8bac9b64047d6636ee5faa280f664902daf06bdd
Flynn Duniho <flynn@harveyosity.org>
2025-09-26 14:59:06 -0700

    Basic features work. Maybe at a "jump to" and a "reset" button?

--- stitch_manager.gd
+++ stitch_manager.gd
@@ -1,26 +1,128 @@
+class_name StitchManager
 extends PanelContainer
 
+@export var transition_duration := 0.2
+@export var stitch_spacing: float = 150
 @export var row_label: Label
-@export var stitch_label: Label
+@export var curr_stitch_label: Label
+@export var next_stitch_label: Label
+@export var next_next_stitch_label: Label
+@export var prev_stitch_label: Label
+@export var row_progress_label: Label
 
-var stitches := BaseStitch.new()
+var index := 0:
+	set(val):
+		index = min(stitches.length, max(val, 0))
+		if stitches.length == 0:
+			prev_stitch_label.text = "-"
+			curr_stitch_label.text = "-"
+			next_stitch_label.text = "-"
+			row_progress_label.text = ""
+			return
+		row_progress_label.text = (
+			"(%d/%d)" % [min(stitches.length - 1, max(next_index, 0)) + 1, stitches.length]
+		)
+		if index == 0:
+			prev_stitch_label.text = "⥒"
+		else:
+			prev_stitch_label.text = str(stitches.child(index - 1))
 
+		if index == stitches.length - 1:
+			next_stitch_label.text = "⥓"
+		elif index == stitches.length:
+			next_stitch_label.text = ""
+		else:
+			next_stitch_label.text = str(stitches.child(index + 1))
 
-func _ready() -> void:
-	stitches.children.append(SingleStitch.new("sc"))
-	stitches.children.append(SingleStitch.new("sc1"))
-	stitches.children.append(SingleStitch.new("sc2"))
-	stitches.children.append(RepeatStitch.new(SingleStitch.new("sc"), 5))
-	print(stitches)
-	print(stitches.child(4))
+		if index == stitches.length - 2:
+			next_next_stitch_label.text = "⥓"
+		elif index >= stitches.length - 1:
+			next_next_stitch_label.text = ""
+		else:
+			next_next_stitch_label.text = str(stitches.child(index + 2))
+
+		if index == stitches.length:
+			curr_stitch_label.text = "⥓"
+		else:
+			curr_stitch_label.text = str(stitches.child(index))
+
+var smooth_index: float = 0:
+	set(val):
+		smooth_index = min(stitches.length, max(val, 0))
+		index = floor(smooth_index)
+		var rect := get_viewport_rect()
+		var parent: Control = curr_stitch_label.get_parent()
+		var center := rect.size.x / 2
+		var offset := smooth_index - index + 0.5
+
+		prev_stitch_label.scale = (Vector2.ONE * (smoothstep(1, -1, smooth_index - index)))
+		curr_stitch_label.scale = (Vector2.ONE * (smoothstep(2, 0, smooth_index - index)))
+		next_stitch_label.scale = (Vector2.ONE * (smoothstep(-1, 1, smooth_index - index)))
+		next_next_stitch_label.scale = (Vector2.ONE * (smoothstep(0, 2, smooth_index - index)))
+
+		curr_stitch_label.position.x = (
+			center - offset * stitch_spacing * (curr_stitch_label.scale.x + 1) / 2
+		)
+		prev_stitch_label.position.x = (
+			center - (offset + 1) * stitch_spacing * (prev_stitch_label.scale.x + 1) / 2
+		)
+		next_stitch_label.position.x = (
+			center + stitch_spacing - offset * stitch_spacing * (next_stitch_label.scale.x + 1) / 2
+		)
+		next_next_stitch_label.position.x = (
+			center
+			+ stitch_spacing * 1.75
+			- offset * stitch_spacing * (next_next_stitch_label.scale.x + 1) / 2
+		)
+
+		curr_stitch_label.position.y = (
+			parent.size.y / 2 - curr_stitch_label.size.y * curr_stitch_label.scale.y / 2
+		)
+		prev_stitch_label.position.y = (
+			parent.size.y / 2 - prev_stitch_label.size.y * prev_stitch_label.scale.y / 2
+		)
+		next_stitch_label.position.y = (
+			parent.size.y / 2 - next_stitch_label.size.y * next_stitch_label.scale.y / 2
+		)
+		next_next_stitch_label.position.y = (
+			parent.size.y / 2 - next_next_stitch_label.size.y * next_next_stitch_label.scale.y / 2
+		)
+
+var next_index := 0
+var curr_tween: Tween
+
+var stitches := BaseStitch.new():
+	set(val):
+		stitches = val
+		smooth_index = min(index, stitches.length)
 
 
 func next_stitch() -> void:
-	stitch_label.text = "New stitch"
+	if curr_tween != null and curr_tween.is_running():
+		curr_tween.kill()
+		smooth_index = next_index
+	next_index = index + 1
+	curr_tween = create_tween()
+	(
+		curr_tween
+		. tween_property(self, "smooth_index", next_index, transition_duration)
+		. set_trans(Tween.TRANS_EXPO)
+		. set_ease(Tween.EASE_OUT)
+	)
 
 
 func prev_stitch() -> void:
-	stitch_label.text = "Old stitch"
+	if curr_tween != null and curr_tween.is_running():
+		curr_tween.kill()
+		smooth_index = next_index
+	next_index = index - 1
+	curr_tween = create_tween()
+	(
+		curr_tween
+		. tween_property(self, "smooth_index", next_index, transition_duration)
+		. set_trans(Tween.TRANS_EXPO)
+		. set_ease(Tween.EASE_OUT)
+	)
 
 
 func update_row(row: String) -> void:
@@ -68,7 +170,9 @@ func update_row(row: String) -> void:
 				stack[-1].children.append(p)
 			"x" when i < row.length() - 1 and row[i + 1] == " ":
 				pass
-			"1", "2", "3", "4", "5", "6", "7", "8", "9", "0":
+			"1", "2", "3", "4", "5", "6", "7", "8", "9", "0" when (
+				(i > 0 and row[i - 1] == " ") or strings[1] != ""
+			):
 				strings[1] += curr
 			",":
 				add_single_stitch.call()
