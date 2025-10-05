144d64df3358cd5f272fc01b2e5067fb6e9cf288
Flynn Duniho <flynn@harveyosity.org>
2025-10-04 22:29:48 -0700

    Make it more mobile-friendly

--- stitch_types/repeat_stitch.gd
+++ stitch_types/repeat_stitch.gd
@@ -1,22 +1,35 @@
 class_name RepeatStitch
 extends BaseStitch
 
+var repeat: int = 0
 
-func _init(c: BaseStitch, repeat: int) -> void:
-    children = []
-    children.resize(repeat)
-    children.fill(c)
+
+func _init(c: BaseStitch, rpt: int) -> void:
+    children = [c]
+    repeat = rpt
+
+
+func _get_length() -> int:
+    if repeat == 0:
+        return 0
+    return children[0].length * repeat
 
 
 func _to_string() -> String:
     if children.size() == 0:
         return "EMPTY"
-    return str(children[0], " x", children.size())
+    return str(children[0], " x", repeat)
+
+
+func child(idx: int) -> SingleStitch:
+    if repeat == 0:
+        return null
+    return children[0].child(idx % children[0].length)
 
 
 func debug_to_string(indent: int = 0) -> String:
     var space := _get_debug_spacing(indent)
     return (
 		"%sRepeat stitch [\n%s\n%s] (x%d)"
-        % [space, children[0].debug_to_string(indent + 1), space, children.size()]
+        % [space, children[0].debug_to_string(indent + 1), space, repeat]
     )
