dad2f8bd79b530b695ff2593575fc685eda3b233
Flynn Duniho <flynn@harveyosity.org>
2025-10-06 08:10:16 -0700

    Reexport after merge

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759763382|204792674';
+const CACHE_VERSION = '1759763413|235779522';
 /** @type {string} */
 const CACHE_PREFIX = 'CrochetCounter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
